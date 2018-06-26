40606d18be3e22c4201057be8a8fed1aa42ce94f
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-26 18:16:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-  POST '/food' do
+  post'/food' do
     "My name is #{params[:name]} and I like to eat #{params[:favorite_food]}."
   end
 end
